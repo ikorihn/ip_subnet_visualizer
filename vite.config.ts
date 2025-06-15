c9d8c05f22f96b7694b6dee3989cf338f311bbf0
ikorihn <16367098+ikorihn@users.noreply.github.com>
2025-06-16 00:12:01 +0900

    Publish to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/ip_subnet_visualizer/',
   test: {
     globals: true,
     environment: 'jsdom',
